6d43b83b723c38655f75a56281ab07fa1eefa823
sebaslh12 <sebaslh12@gmail.com>
2020-11-12 15:05:54 -0500

    feat(login): add login
    Create App context.

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,16 +1,24 @@
-import React from "react"
-import { PostsList } from "/imports/ui/posts/PostsList";
+import React, { useState } from "react"
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { PostsList } from "/imports/ui/posts/PostsList";
 import { CreatePost } from "/imports/ui/posts/CreatePost";
+import { Login } from "/imports/ui/auth/Login";
+import { AppContext } from "/imports/context";
 
 const client = new ApolloClient({
-  uri: "http://localhost:3000/graphql",
-  cache: new InMemoryCache()
+	uri: "http://localhost:3000/graphql",
+	cache: new InMemoryCache()
 });
 
-export const App = () => (
-	<ApolloProvider client={client}>
-		<PostsList />
-		<CreatePost />
-	</ApolloProvider>
-);
+export const App = () => {
+	const [userData, setUserData] = useState({});
+	return (
+		<AppContext.Provider value={{ userData, setUserData }}>
+			<ApolloProvider client={client}>
+				<Login />
+				<PostsList />
+				<CreatePost />
+			</ApolloProvider>
+		</AppContext.Provider>
+	);
+};
